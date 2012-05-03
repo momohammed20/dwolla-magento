b395624120a919a12816b6515f2aea8cef70e976
Michael Schonfeld <michael@schonfeld.org>
2012-05-02 22:04:26 -0400

    first commit. woot.

---
+++ app/local/Dwolla/DwollaPaymentModule/Helper/Data.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Dwolla Payment Module for Magento > 1.5.0
+ *
+ * @category    Dwolla
+ * @package     Dwolla_DwollaPaymentModule
+ * @copyright   Copyright (c) 2012 Dwolla Inc. (http://www.dwolla.com)
+ * @autor   	Michael Schonfeld <michael@dwolla.com>
+ * @version   	2.0.0alpha
+ * @license     http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+
+class Dwolla_DwollaPaymentModule_Helper_Data extends Mage_Core_Helper_Abstract
+{
+
+}
